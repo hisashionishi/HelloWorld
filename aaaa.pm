6c23c682da244ce5b62d1f4be3aa3089b3537d0d
onishi <onishi_hisashi@mhsolutions.co.jp>
2018-03-29 09:13:01 +0900

    aaaa.pmをスクリプトっぽく修正

--- aaaa.pm
+++ aaaa.pm
@@ -1,27 +1,39 @@
-=pod
+package HelloWorld;
+
+use strict;
+use warnings;
 
 =encoding utf8
 
-=head1 タイトルです
+=head1 NAME
 
-=over 4
+Hello worldスクリプト
 
-=item 1
+=head1 DESCRIPTION
 
-項目1
+Hello worldを出力するスクリプト
 
-=item 2
-
-項目2
-
-=item 3
-
-項目3
-
-=item 4
-
-項目4
-
-=back
+=head1 METHODS
 
 =cut
+
+=head2 say
+
+=head3 DESCRIPTION
+
+Hello worldを出力する
+
+=head3 PARAMETERS
+
+=over 1
+
+=item *
+
+input  ユーザー名
+
+=cut
+
+sub say {
+  my $user_name = shift;
+  print "$user_name : Hello world!\n"
+}
